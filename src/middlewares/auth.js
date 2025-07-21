43976029552c35d23518881750777c53a599d50c
apurvM790 <97803586+apurvM790@users.noreply.github.com>
2025-07-21 23:03:48 +0530

    update

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -3,6 +3,7 @@ const User = require("../models/user");
 
 const userAuth = async (req,res,next)=>{
     try {
+        console.log("Cookies: ", req.cookies);
         const { token } = req.cookies;
 
         if(!token){
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -86,7 +86,12 @@ authRouter.post("/login",async (req,res)=>{
 
 // Logout user..
 authRouter.post("/logout", async (req,res)=>{
-    res.cookie("token", null, { expires : new Date(Date.now())});
+    res.cookie("token", null, {
+        expires: new Date(Date.now()),
+        httpOnly: true,
+        sameSite: "None",     
+        secure: true          
+    });
 
     res.send("Logout successfull!");
 });
