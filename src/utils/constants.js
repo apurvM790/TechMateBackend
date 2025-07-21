c0cb27606196c3a14616c60d6c24d4b0f991f2bc
apurvM790 <97803586+apurvM790@users.noreply.github.com>
2025-07-21 18:53:44 +0530

    updated

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 const MALE_URL = "https://w1.pngwing.com/pngs/743/500/png-transparent-circle-silhouette-logo-user-user-profile-green-facial-expression-nose-cartoon-thumbnail.png";
 const FEMALE_URL = "https://w7.pngwing.com/pngs/129/292/png-transparent-female-avatar-girl-face-woman-user-flat-classy-users-icon-thumbnail.png";
-const BASE_URL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://techmate-frontend.vercel.app";
+const BASE_URL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://tech-mate-frontend.vercel.app/";
 module.exports = { MALE_URL, FEMALE_URL, BASE_URL}
